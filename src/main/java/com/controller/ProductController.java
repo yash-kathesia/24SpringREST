2b014959efa469ceb16b6c63ab0d454d3c33da93
yash-kathesia <yashkathesia@gmail.com>
2024-09-13 19:03:19 +0530

    postman + Repository added for products

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,19 +1,26 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.ProductEntity;
+import com.repository.ProductRepository;
 
 @RestController
 public class ProductController {
 
+	@Autowired
+	ProductRepository productRepository;
+
 	@PostMapping("/products")
-	public String addProduct(@RequestBody ProductEntity productEntity) {
-		System.out.println("Name : " + productEntity.getProductName());
-		System.out.println("Price : " + productEntity.getPrice());
-		return "Success";
+	public ProductEntity addProduct(@RequestBody ProductEntity productEntity) {
+//		System.out.println("Name : " + productEntity.getProductName());
+//		System.out.println("Price : " + productEntity.getPrice());
+
+		productRepository.save(productEntity);
+		return productEntity;
 	}
 
 }
