20a1f9f1a09b966e25267e5eabb65d7f54170938
yash-kathesia <yashkathesia@gmail.com>
2024-09-18 19:07:51 +0530

    Get All Products & Find By ID

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,8 +1,14 @@
 package com.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.ProductEntity;
@@ -23,4 +29,32 @@ public class ProductController {
 		return productEntity;
 	}
 
+	@GetMapping("/products")
+	public List<ProductEntity> getAllProducts() {
+		List<ProductEntity> products = productRepository.findAll();
+		return products;
+	}
+
+	@GetMapping("/products/{id}")
+	public ProductEntity getProductById(@PathVariable("id") Integer productId) {
+		Optional<ProductEntity> op = productRepository.findById(productId);
+		if (op.isEmpty()) {
+			return null;
+		} else {
+			ProductEntity products = op.get();
+			return products;
+		}
+	}
+
+	@GetMapping("/productsById")
+	public ProductEntity getProductBYID(@RequestParam("productId") Integer productId) {
+		Optional<ProductEntity> op = productRepository.findById(productId);
+		if (op.isEmpty()) {
+			return null;
+		} else {
+			ProductEntity productEntity = op.get();
+			return productEntity;
+		}
+	}
+	
 }
