49a8ff702ab9731950550eb3d49be8ed81b35816
yash-kathesia <yashkathesia@gmail.com>
2024-09-24 18:06:55 +0530

    Delete by ID & Update Product

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,5 +58,22 @@ public class ProductController {
 			return productEntity;
 		}
 	}
-	
+
+	@DeleteMapping("/products/{productId}")
+	public String deleteById(@PathVariable("productId") Integer productId) {
+		Optional<ProductEntity> op = productRepository.findById(productId);
+		if (op.isEmpty()) {
+			return "Not Found...!!!";
+		} else {
+			productRepository.deleteById(productId);
+			return "Success...!!!";
+		}
+	}
+
+	@PutMapping("/product")
+	public String updateByID(@RequestBody ProductEntity productEntity) {
+		productRepository.save(productEntity);
+		return "SUCCESSFULLY DONE...!!!";
+	}
+
 }
